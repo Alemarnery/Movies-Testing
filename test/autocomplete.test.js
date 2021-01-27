ec3164a6478657fd78303a4b245a14e1861468fa
alemarnery <aleemar.95@gmail.com>
2021-01-27 17:28:12 -0430

    autocomplete

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,66 +1,61 @@
 const waitFor = (selector) => {
-     return new Promise((resolve,reject) => {
-          const interval = setInterval( () => {
-               if(document.querySelector(selector)){
-                    clearInterval(interval);
-                    clearTimeout(timeout);
-                    resolve();
-               }
-          }, 30);
+  return new Promise((resolve, reject) => {
+    const interval = setInterval(() => {
+      if (document.querySelector(selector)) {
+        clearInterval(interval);
+        clearTimeout(timeout);
+        resolve();
+      }
+    }, 30);
 
-          const timeout = setTimeout(() => {
-               clearInterval(interval);
-               reject();
-          }, 2000);
-     });
-}
-
+    const timeout = setTimeout(() => {
+      clearInterval(interval);
+      reject();
+    }, 2000);
+  });
+};
 
 beforeEach(() => {
-     document.querySelector('#target').innerHTML = '';
+  document.querySelector("#target").innerHTML = "";
 
-     createAutoComplete({
-          root: document.querySelector('#target'),
-          fetchData() {
-               return [
-                    { Title: 'Avengers'},
-                    { Title: 'Not Avengers'},
-                    { Title: 'Some other movie'}
-               ];
-          },
-          renderOption(movie) {
-               return movie.Title;     
-          }
-     });
-
+  createAutoComplete({
+    root: document.querySelector("#target"),
+    fetchData() {
+      return [
+        { Title: "Avengers" },
+        { Title: "Not Avengers" },
+        { Title: "Some other movie" },
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    },
+  });
 });
 
-it('Dropdown starts closed', () => {
-     const dropdown = document.querySelector('.dropdown');
-     expect(dropdown.className).not.to.include('is-active');
+it("Dropdown starts closed", () => {
+  const dropdown = document.querySelector(".dropdown");
+  expect(dropdown.className).not.to.include("is-active");
 });
 
-it('After searching, dropdown opens up', async () => {
+it("After searching, dropdown opens up", async () => {
+  const input = document.querySelector("input");
+  input.value = "avengers";
+  input.dispatchEvent(new Event("input"));
 
-     const input = document.querySelector('input');
-     input.value = 'avengers';
-     input.dispatchEvent( new Event('input')  );
+  await waitFor(".dropdown-item");
 
-     await waitFor('.dropdown-item');
-
-     const dropdown = document.querySelector('.dropdown');
-     expect(dropdown.className).to.include('is-active');     
-
+  const dropdown = document.querySelector(".dropdown");
+  expect(dropdown.className).to.include("is-active");
 });
 
-it('After searcing, displays some results', async() => {
+it("After searcing, displays some results", async () => {
+  const input = document.querySelector("input");
+  input.value = "avengers";
+  input.dispatchEvent(new Event("input"));
 
-     const input = document.querySelector('input');
-     input.value = 'avengers';
-     input.dispatchEvent( new Event('input')  );
+  await waitFor(".dropdown-item");
 
-     await waitFor('.dropdown-item');
-
-     const items = document.querySelectorAll('.dropdown-item');
-     expect(items.length).to.equal(3); 
+  const items = document.querySelectorAll(".dropdown-item");
+  expect(items.length).to.equal(3);
 });
